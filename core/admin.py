42ccaf1fd07e28abddade2b63f8ea8c58f86b5d1
Bernd <3483909+bzaiser@users.noreply.github.com>
2026-04-02 21:31:48 +0200

    polish: german translation, avatar fix and admin color pickers

--- core/admin.py
+++ core/admin.py
@@ -9,10 +9,24 @@ class CustomUserAdmin(UserAdmin):
     inlines = [CashFlowSourceInline]
 
 
+from django import forms
+
+class ColorPickerWidget(forms.TextInput):
+    def __init__(self, attrs=None):
+        default_attrs = {'type': 'color', 'class': 'form-control-color'}
+        if attrs:
+            default_attrs.update(attrs)
+        super().__init__(default_attrs)
+
 @admin.register(Theme)
 class ThemeAdmin(admin.ModelAdmin):
     list_display = ('name', 'primary_color', 'background_color')
     search_fields = ('name',)
+    
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name.endswith('_color') or db_field.name.startswith('gradient_'):
+            kwargs['widget'] = ColorPickerWidget
+        return super().formfield_for_dbfield(db_field, **kwargs)
 
 @admin.register(UserProfile)
 class UserProfileAdmin(BaseOwnedModelAdmin):
